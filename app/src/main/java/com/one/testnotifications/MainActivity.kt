8dfb1a0b0482516460adb0c94e7588b76013e773
paparym <paparym47@gmail.com>
2021-12-13 22:07:23 +0200

    to be checked rx

--- app/src/main/java/com/one/testnotifications/MainActivity.kt
+++ app/src/main/java/com/one/testnotifications/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.one.testnotifications
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.messaging.FirebaseMessaging
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val observable = Observable.just(1, 2, 3)
+
+//        val disposte = observable.subscribe() {
+//            Log.d("TAG", )
+//        }
     }
 }
