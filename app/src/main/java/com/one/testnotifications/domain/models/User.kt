70ca3a9e012a67bd725e88793824a5d989e35a9b
paparym <paparym47@gmail.com>
2022-01-06 12:42:35 +0200

    separate to different modules

--- app/src/main/java/com/one/testnotifications/domain/models/User.kt
+++
@@ -1,4 +0,0 @@
-package com.one.testnotifications.domain.models
-
-data class User(val firstName: String, val lastName: String, val age: Int) {
-}
---
+++ domain/src/main/java/com/one/testnotifications/domain/models/User.kt
@@ -0,0 +1,3 @@
+package com.one.testnotifications.domain.models
+
+data class User(val firstName: String, val lastName: String, val age: Int)
